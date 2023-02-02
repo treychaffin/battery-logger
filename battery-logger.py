095e50daca2e81564a4a11a10ef91a02423f574d
treychaffin <treychaffin@wildcatfabrication.com>
2023-02-02 12:41:53 -0600

    initial python script

---
+++ battery-logger.py
@@ -0,0 +1,38 @@
+import time 
+import csv
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+#initialize ADS1115
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+ads.gain = 1
+
+#set channel A0
+chan = AnalogIn(ads, ADS.P0)
+
+r1 = 47000
+r2 = 10000
+vcal = 0.994423
+
+def volt_divider():
+    return round(chan.voltage*vcal*(r1+r2)/r2,2)
+
+#get the current date and time
+curr_time = time.strftime("%Y-%m-%d-%H-%M-%S")
+
+#get current date
+curr_date = time.strftime("%Y-%m-%d")
+
+#construct filename
+filename = curr_date + ".csv"
+
+#get the voltage from the channel
+voltage = volt_divider()
+
+#write the voltage and time to a csv file
+with open(filename, 'w', newline='') as csvfile:
+    csvfile.writer(csvfile, delimiter=',')
+    csvfile.writerow({curr_time,voltage})
